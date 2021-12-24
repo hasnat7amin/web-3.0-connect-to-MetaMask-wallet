bd4be1e6b3b083135c9aa07a5dae3c4076accb7f
Hasnat Amin <hasnatamin065@gmail.com>
2021-12-25 00:07:47 +0500

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Web3 from "web3";
+import { Web3ReactProvider } from "@web3-react/core";
 
+function getLibrary(provider) {
+  return new Web3(provider);
+}
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Component {...pageProps} />
+    </Web3ReactProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
